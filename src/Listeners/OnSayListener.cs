79847577a5abfbf084b0159432dd1ca8828c38d8
ShookEagle <shookeagle@gmail.com>
2026-04-07 19:48:32 -0500

    Refactor OnSayListener to replace user message hook with input command handling, since MAUL is too obtrusive

--- src/Listeners/OnSayListener.cs
+++ src/Listeners/OnSayListener.cs
@@ -1,30 +1,29 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.UserMessages;
 using RMenu.Enums;
 
 namespace RMenu.Listeners;
 
 internal static class OnSayListener {
   public static void Register() {
-    NativeAPI.HookUsermessage(118,
-      (InputArgument)FunctionReference.Create(OnSay), HookMode.Pre);
+    NativeAPI.AddCommand("css_input", // your command
+      "Menu input", false, (int)ConCommandFlags.FCVAR_LINKED_CONCOMMAND,
+      FunctionReference.Create(OnInputCommand));
   }
 
-  private static HookResult OnSay(UserMessage um) {
-    var index   = um.ReadInt("entityindex");
-    var message = um.ReadString("param2");
 
-    if (Utilities.GetPlayerFromIndex(index) is not { IsValid: true } player
-      || Menu.Get(player) is not { Text                     : true } menu
+  private static void OnInputCommand(int playerSlot, IntPtr commandInfo) {
+    var caller = playerSlot != -1 ?
+      Utilities.GetPlayerFromSlot(playerSlot) :
+      null;
+    if (caller is not { IsValid        : true } player
+      || Menu.Get(player) is not { Text: true } menu
       || menu.SelectedItem?.Item is not { } item)
-      return HookResult.Continue;
+      return;
 
-    item.Data = message;
+    item.Data = NativeAPI.CommandGetArgByIndex(commandInfo, 1).Trim('"');
 
     menu.Text = false;
     menu.invoke(MenuAction.INPUT);
-
-    return HookResult.Continue;
   }
 }
